23414e84124b7a5f0f8b638dcef916faf31594cb
Rocky Jaiswal <rockyj@rockyj-home-main.fritz.box>
2018-02-11 19:35:43 +0100

    More optimization.

--- lib/euler_5.ex
+++ lib/euler_5.ex
@@ -1,11 +1,11 @@
 defmodule Euler_5 do
 
   defp is_divisible_by_all(n) do
-    Enum.all?([7, 11, 13, 16, 17, 18, 19, 20], fn(x) -> rem(n, x) == 0 end)
+    Enum.all?([2520, 11, 13, 16, 17, 19], fn(x) -> rem(n, x) == 0 end)
   end
 
   def smallest_multiple do
-    2000..500_000_000
+    50400..500_000_000
       |> Enum.find(fn(x) -> is_divisible_by_all(x) end)
   end
 
