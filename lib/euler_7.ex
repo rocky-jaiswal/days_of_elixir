11785740a848a895ed19d88427ebb6d93dc8c6b6
Jaiswal, Rocky (McKinsey) <jaiswal@mediamarktsaturn.com>
2018-02-12 21:18:08 +0100

    A more parallel version.

--- lib/euler_7.ex
+++ lib/euler_7.ex
@@ -1,35 +1,30 @@
 defmodule Euler_7 do
 
-  defp mutate_state(x, num, count \\ 0, acc \\ []) do
+  defp filter_non_primes(x, num, count \\ 0, acc \\ []) do
     n = :math.pow(x, 2) + (x * count)
     if n <= num do
       acc = acc ++ [n]
-      mutate_state(x, num, count + 1, acc)
+      filter_non_primes(x, num, count + 1, acc)
     else
       acc
     end
   end
 
+  defp pmap(collection, num) do
+    collection
+      |> Enum.map(&(Task.async(fn -> filter_non_primes(&1, num) end)))
+      |> Enum.map(&Task.await/1)
+  end
+
   def prime_sieve do
-    num = 100
+    num = 10000
     sqrt = trunc(Float.ceil :math.sqrt num)
 
-    state = 2..num
-      |> Enum.reduce(%{}, fn (x, acc) -> Map.put(acc, x, true) end)
-
-    non_primes = 2..sqrt
-      |> Enum.flat_map(
-        fn (x) ->
-          if Map.fetch state, x do
-            mutate_state(trunc(x), num)
-          else
-            []
-          end
-        end
-      )
+    non_primes = pmap(2..sqrt, num)
+      |> List.flatten
       |> Enum.map(fn (x) -> trunc(x) end)
 
-    Enum.sort(Map.keys(state) -- non_primes)
+    Enum.to_list(2..num) -- non_primes
   end
 
 end
